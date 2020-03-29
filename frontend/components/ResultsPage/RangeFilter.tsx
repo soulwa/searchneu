6312fcf1d1f8ae5bdd0af1dc6dfab4dc436c29a0
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-03-29 11:29:46 -0400

    Change CSS to follow BEM naming conventions (I think).

--- frontend/components/ResultsPage/RangeFilter.tsx
+++ frontend/components/ResultsPage/RangeFilter.tsx
@@ -10,19 +10,19 @@ interface RangeFilterProps {
 export default function RangeFilter({ title, selected, setActive }: RangeFilterProps) {
   return (
     <div className='RangeFilter'>
-      <div className='filter__title'>
+      <div className='RangeFilter__title'>
         <p>
           {title}
         </p>
       </div>
-      <div className='range-input'>
-        <div className='range-min'>
-          <label className='label-min'>From: </label>
-          <input type='string' className='input-box' value={ selected.min } onChange={ (event) => setActive({ min:Number(event.target.value), max:selected.max }) } />
+      <div className='RangeFilter__input'>
+        <div className='RangeFilter__range-min'>
+          <label className='RangeFilter__label-min'>From: </label>
+          <input type='string' className='RangeFilter__input-box' value={ selected.min } onChange={ (event) => setActive({ min:Number(event.target.value), max:selected.max }) } />
         </div>
         <div>
-          <label className='label-max'>To: </label>
-          <input type='string' className='input-box' value={ selected.max } onChange={ (event) => setActive({ min: selected.min, max:Number(event.target.value) }) } />
+          <label className='RangeFilter__label-max'>To: </label>
+          <input type='string' className='RangeFilter__input-box' value={ selected.max } onChange={ (event) => setActive({ min: selected.min, max:Number(event.target.value) }) } />
         </div>
       </div>
     </div>
