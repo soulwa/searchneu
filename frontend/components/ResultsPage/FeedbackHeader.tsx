1d676fac8ca31d67f47186e62155efa6d28a5b72
Amiel Monasterial <amielmon@gmail.com>
2020-04-04 12:58:34 -0400

    mobile styles and does something

--- frontend/components/ResultsPage/FeedbackHeader.tsx
+++ frontend/components/ResultsPage/FeedbackHeader.tsx
@@ -1,37 +1,74 @@
-import React, { useState } from "react";
-import macros from "../macros";
+import React, { useState } from 'react';
+import macros from '../macros';
 
 export default function FeedbackHeader() {
-    const [close, setClose] = useState(false);
-    const [yes, setYes] = useState(false);
-    const [no, setNo] = useState(false);
+  const [close, setClose] = useState(false);
+  const [yes, setYes] = useState(false);
+  const [no, setNo] = useState(false);
+  const [feedbackQuery, setFeedbackQuery] = useState('');
 
-    return (
-       !close && (<>
-        <div className="FeedbackHeader">
-          { !yes && !no &&  
-            (<>
-            <label className="FeedbackHeader__q1">
-                Did you find what you're looking for?
-            </label>
-            <div className="FeedbackHeader__yes" role="button" onClick={() => setYes(true)}>
-                {macros.isMobile ? "Y" : "Yes"}
-            </div>
-            <div className="FeedbackHeader__no" role="button" onClick={() => setNo(true)}>
-            {macros.isMobile ? "N" : "No"}
-            </div>
+  return (
+    !close && (
+      <>
+        <div className='FeedbackHeader'>
+          { !yes && !no
+            && (
+              <>
+                <label className='FeedbackHeader__q1'>
+                  Did you find what you&apos;re looking for?
+                </label>
+                <div
+                  className='FeedbackHeader__yes'
+                  role='button'
+                  tabIndex={ 0 }
+                  onClick={ () => {
+                    setYes(true);
+                    macros.logAmplitudeEvent('Feedback header click yes', { message: 'user found what they were looking for' });
+                  } }
+                >
+                  {macros.isMobile ? 'Y' : 'Yes'}
+                </div>
+                <div
+                  className='FeedbackHeader__no'
+                  role='button'
+                  tabIndex={ 0 }
+                  onClick={ () => {
+                    setNo(true);
+                    macros.logAmplitudeEvent('Feedback header click no', { message: 'user did not find what they were looking for' });
+                  } }
+                >
+                  {macros.isMobile ? 'N' : 'No'}
+                </div>
+              </>
+            )}
+          {yes && <div className='FeedbackHeader__thanks'>Thank you for helping make SearchNEU better!</div> }
+          {no && (
+            <>
+              <div className='FeedbackHeader__q2'>What were you looking for?</div>
+              <input
+                className='FeedbackHeader__input'
+                autoComplete='off'
+                // eslint-disable-next-line jsx-a11y/no-autofocus
+                autoFocus={ !macros.isMobile }
+                placeholder={ !macros.isMobile ? 'Enter your original query' : undefined }
+                value={ feedbackQuery }
+                onChange={ (event) => { setFeedbackQuery(event.target.value); } }
+                onKeyDown={ (event) => {
+                  if (event.key === 'Enter') {
+                    setYes(true);
+                    setNo(false);
+                    console.log('feedbackQuery', feedbackQuery);
+                    macros.logAmplitudeEvent('Feedback header user enter input', { message: feedbackQuery })
+                  }
+                } }
+              />
             </>
-            )
-          }
-          {yes && <div className="FeedbackHeader__thanks">Thank you for helping make SearchNEU better!</div> }
-          {no && (<>
-                <div className="FeedbackHeader__q2">What were you looking for?</div>
-                <input className="FeedbackHeader__input"/>
-                </>)}
-            <div className="FeedbackHeader__close" onClick={() => setClose(true)} role="button"/>
+          )}
+          <div className='FeedbackHeader__close' onClick={ () => setClose(true) } role='button' aria-label='close' tabIndex={ 0 } />
         </div>
-        <div className="FeedbackHeader__padding"/>
-        </>) 
+        <div className='FeedbackHeader__padding' />
+      </>
+    )
 
-    );
+  );
 }
