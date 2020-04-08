35de78b704803ba2238ba0962cad67974a1c0831
dajinchu <dajinchu@gmail.com>
2020-04-08 02:08:05 -0400

    scripts

--- scripts/load_data.sh
+++
@@ -1,2 +0,0 @@
-curl -L -o public/data/v2/getTermDump/allTerms.json searchneu.com/data/v2/getTermDump/allTerms.json
-curl -L -o public/data/v2/employeeDump.json searchneu.com/data/v2/employeeDump.json
