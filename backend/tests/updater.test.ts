ab290087ed6dabd99b962958d42709ada353d58c
gamburgm <mitch.gamburg@gmail.com>
2020-04-08 16:08:48 -0400

    tests

--- backend/tests/updater.test.ts
+++ backend/tests/updater.test.ts
@@ -7,12 +7,18 @@ import dumpProcessor from '../dumpProcessor';
 import termParser from '../scrapers/classes/parsersxe/termParser';
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   jest.spyOn(notifyer, 'sendFBNotification').mockImplementation(() => {});
+  jest.useFakeTimers();
   await Course.truncate({ cascade: true, restartIdentity: true });
   await Section.truncate({ cascade: true, restartIdentity: true });
   await User.truncate({ cascade: true, restartIdentity: true });
   await FollowedCourse.truncate({ cascade: true, restartIdentity: true });
   await FollowedSection.truncate({ cascade: true, restartIdentity: true });
+});
+
+afterEach(() => {
+  jest.clearAllTimers();
 });
 
 afterAll(async () => {
@@ -133,12 +139,21 @@ describe('Updater', () => {
   });
 
   describe('generateCourseMsg', () => {
-    it('generates the correct message', () => {
+    it('generates a message for multiple sections getting added', () => {
       const userToMsg: Record<string, string[]> = {};
       UPDATER.generateCourseMsg(['user1', 'user2'], { type: 'Course', course: FUNDIES_ONE, count: 2 }, userToMsg);      
       expect(userToMsg).toEqual({
         'user1': ['2 sections were added to CS2500! Check it out at https://searchneu.com/202030/CS2500 !'],
         'user2': ['2 sections were added to CS2500! Check it out at https://searchneu.com/202030/CS2500 !'],
+      });
+    });
+
+    it('generates a message for a single class notification', () => {
+      const userToMsg: Record<string, string[]> = {};
+      UPDATER.generateCourseMsg(['user1', 'user2'], { type: 'Course', course: FUNDIES_ONE, count: 1 }, userToMsg);
+      expect(userToMsg).toEqual({
+        'user1': ['A section was added to CS2500! Check it out at https://searchneu.com/202030/CS2500 !'],
+        'user2': ['A section was added to CS2500! Check it out at https://searchneu.com/202030/CS2500 !'],
       });
     });
   });
@@ -164,10 +179,10 @@ describe('Updater', () => {
   });
 
   describe('sendMessages', () => {
-    it('sends correct messages', () => {
-      const classHash: Record<string, string[]> = { 'neu.edu/202030/CS/2500': ['user1', 'user2'], 'neu.edu/202030/CS/2510': ['user2'], 'neu.edu/202030/CS/4400': [] };
-      const sectionHash: Record<string, string[]> = { 'neu.edu/202030/CS/2500/5678': ['user1', 'user2'], 'neu.edu/202030/CS/2510/0248': ['user2'], 'neu.edu/202030/CS/2510/1357': ['user2'], 'neu.edu/202030/CS/4400/0987': [] };
+    const classHash: Record<string, string[]> = { 'neu.edu/202030/CS/2500': ['user1', 'user2'], 'neu.edu/202030/CS/2510': ['user2'], 'neu.edu/202030/CS/4400': [] };
+    const sectionHash: Record<string, string[]> = { 'neu.edu/202030/CS/2500/5678': ['user1', 'user2'], 'neu.edu/202030/CS/2510/0248': ['user2'], 'neu.edu/202030/CS/2510/1357': ['user2'], 'neu.edu/202030/CS/4400/0987': [] };
 
+    it('sends correct messages', () => {
       const notifications: Notification[] = [
         { type: 'Course', course: FUNDIES_ONE, count: 1 },
         { type: 'Section', section: FUNDIES_ONE_S2 },
@@ -186,21 +201,15 @@ describe('Updater', () => {
         ['user2', 'A seat opened up in CS2510 (CRN: 1357). Check it out at https://searchneu.com/202030/CS2510 !'],
       ]);
     });
+
+    it('does not send any messages if there are no notifications', () => {
+      UPDATER.sendMessages([], classHash, sectionHash);
+      expect(notifyer.sendFBNotification.mock.calls).toEqual([]);
+    });
   });
 
   describe('update', () => {
-    it.only('WORKS', async () => {
-      jest.spyOn(dumpProcessor, 'main').mockImplementation(() => {});
-      jest.spyOn(termParser, 'requestsSectionsForTerm').mockImplementation(() => {
-        return [
-          FUNDIES_ONE_S1,
-          FUNDIES_ONE_S2,
-          FUNDIES_TWO_S1,
-          FUNDIES_TWO_S2,
-          PL_S1,
-        ];
-      });
-
+    beforeEach(async () => {
       await Course.create({ ...FUNDIES_ONE, id: 'neu.edu/202030/CS/2500' });
       await Course.create({ ...FUNDIES_TWO, id: 'neu.edu/202030/CS/2510' });
       await Course.create({ ...PL, id: 'neu.edu/202030/CS/4400' });
@@ -212,6 +221,7 @@ describe('Updater', () => {
         seatsRemaining: 0,
         waitRemaining: 0,
       });
+
       await Section.create({ 
         ...FUNDIES_TWO_S1, 
         id: 'neu.edu/202030/CS/2510/0248', 
@@ -286,8 +296,23 @@ describe('Updater', () => {
         userId: 'user2',
         sectionId: 'neu.edu/202030/CS/2510/1357',
       });
+    });
+
+    it('WORKS', async () => {
+      jest.spyOn(dumpProcessor, 'main').mockImplementation(() => {});
+      jest.spyOn(termParser, 'requestsSectionsForTerm').mockImplementation(() => {
+        return [
+          FUNDIES_ONE_S1,
+          FUNDIES_ONE_S2,
+          FUNDIES_TWO_S1,
+          FUNDIES_TWO_S2,
+          PL_S1,
+        ];
+      });
+
 
       await UPDATER.update();
+      jest.runOnlyPendingTimers();
 
       expect(notifyer.sendFBNotification.mock.calls).toEqual([
         ['user1', 'A section was added to CS2500! Check it out at https://searchneu.com/202030/CS2500 !'],
@@ -296,8 +321,41 @@ describe('Updater', () => {
         ['user2', 'A seat opened up in CS2500 (CRN: 5678). Check it out at https://searchneu.com/202030/CS2500 !'],
         ['user2', 'A waitlist seat has opened up in CS2510 (CRN: 0248). Check it out at https://searchneu.com/202030/CS2510 !'],
         ['user2', 'A seat opened up in CS2510 (CRN: 1357). Check it out at https://searchneu.com/202030/CS2510 !'],
+        ['user1', 'Reply with "stop" to unsubscribe from notifications.'],
+        ['user2', 'Reply with "stop" to unsubscribe from notifications.'],
       ]);
     });
-  });
 
+    it('does not send unnecessary messages', async () => {
+      jest.spyOn(dumpProcessor, 'main').mockImplementation(() => {});
+      jest.spyOn(termParser, 'requestsSectionsForTerm').mockImplementation(() => {
+        return [
+          PL_S1,
+        ];
+      });
+
+
+      await UPDATER.update();
+      jest.runOnlyPendingTimers();
+
+      expect(notifyer.sendFBNotification.mock.calls).toEqual([]);
+    });
+
+    it('does not send messages if scraped classes do not match with followed terms', async () => {
+      jest.spyOn(dumpProcessor, 'main').mockImplementation(() => {});
+      jest.spyOn(termParser, 'requestsSectionsForTerm').mockImplementation(() => {
+        return [
+          { ...FUNDIES_ONE_S2, termId: '202110' },
+          { ...FUNDIES_TWO_S1, termId: '202110' },
+          { ...FUNDIES_TWO_S2, termId: '202110' },
+        ];
+      });
+
+
+      await UPDATER.update();
+      jest.runOnlyPendingTimers();
+
+      expect(notifyer.sendFBNotification.mock.calls).toEqual([]);
+    });
+  });
 });
