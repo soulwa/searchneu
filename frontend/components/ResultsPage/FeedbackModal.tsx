45759661f48bb6774c7d38889af66743c284c045
Amiel Monasterial <amielmon@gmail.com>
2020-04-10 15:09:27 -0400

    modal

--- frontend/components/ResultsPage/FeedbackModal.tsx
+++ frontend/components/ResultsPage/FeedbackModal.tsx
@@ -1,18 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import LogoInput from '../images/LogoInput';
 
 export default function FeedbackModal() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return (
-        <div className="FeedbackModal">
-            <div className="FeedbackModal__pill" onClick={() => setOpen(false)}>
-                <LogoInput height='14' width='18' fill='#d41b2c'/>
-                <p>SearchNEU Feedback</p>
-            </div>
+  return (
+    <div className='FeedbackModal'>
+      {open && (
+        <div className='FeedbackModal__popout'>
+          <div className='FeedbackModal__popoutHeader'>
+            <p>SearchNEU Feedback</p>
+          </div>
+          <div className='FeedbackModal__popoutSubHeader'>
+            <p>What info are you looking for?</p>
+          </div>
         </div>
+      ) }
+      <div className='FeedbackModal__pill' role='button' tabIndex={ 0 } onClick={ () => { setOpen(!open) } }>
+        <LogoInput height='14' width='18' fill='#d41b2c' />
+        <p>SearchNEU Feedback</p>
+      </div>
+    </div>
 
 
-    );
-
+  );
 }
