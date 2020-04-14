3ac378019874d88373872eb44977ea6559b89ef3
dajinchu <dajinchu@gmail.com>
2020-04-14 15:11:53 -0400

    no output github user key

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,8 +1,1 @@
 # outputs.tf
-
-output "github_actions_AWS_ACCESS_KEY_ID" {
-  value = aws_iam_access_key.github_actions_user.id
-}
-output "github_actions_AWS_SECRET_ACCESS_KEY" {
-  value = aws_iam_access_key.github_actions_user.secret
-}
