a0f411e82f769a546699f89e79922e7557633184
gamburgm <mitch.gamburg@gmail.com>
2020-04-16 17:03:34 -0400

    working tests

--- common/jestSetupFile.js
+++ common/jestSetupFile.js
@@ -7,16 +7,18 @@ import 'mock-local-storage';
 import 'regenerator-runtime/runtime';
 
 
-if (process.env.NODE_ENV === 'test') {
-  global.requestAnimationFrame = (callback) => {
-    setTimeout(() => callback(), 0);
-  };
+if (process.env.PROD !== 'true') {
+  if (process.env.NODE_ENV === 'test') {
+    global.requestAnimationFrame = (callback) => {
+      setTimeout(() => callback(), 0);
+    };
 
-  window.ga = function ga() {
+    window.ga = function ga() {
 
-  };
+    };
+  }
+
+  // Mock out this file on all tests/
+  // This file is a data-abstraction-layer (aka wrapper) around window.FB, which does not exist in TEST
+  jest.mock('../frontend/components/facebook');
 }
-
-// Mock out this file on all tests/
-// This file is a data-abstraction-layer (aka wrapper) around window.FB, which does not exist in TEST
-jest.mock('../frontend/components/facebook');
