1af8819e7db32b75db7b61555c646d11ca0a6c75
dajinchu <dajinchu@gmail.com>
2020-04-19 13:53:54 -0400

    better error

--- backend/generateSitemap.ts
+++ backend/generateSitemap.ts
@@ -25,35 +25,37 @@ export default async function generateSitemap(): Promise<string> {
     limit: 1,
     raw: true,
   }));
-  if (terms.length > 0) {
-    const currentTerm = terms[0].termId;
-
-    macros.log('The current term is:', currentTerm);
-
-    // Add the classes
-    const courses = await Course.findAll({
-      attributes: ['subject', 'classId', 'name'],
-      where: { termId: currentTerm },
-      raw: true,
-    });
-    for (const course of courses) {
-      items.push(`${course.subject} ${course.classId}`);
-      items.push(course.name);
-    }
-
-    // Add the employees
-    const employees = await Professor.findAll({
-      attributes: ['name'],
-      benchmark: true,
-      raw: true,
-    });
-    for (const employee of employees) {
-      items.push(employee.name);
-    }
-
-    // Remove duplicates
-    items = Array.from(new Set(items));
+  if (terms.length === 0) {
+    macros.error('could not generate sitemap');
+    return '';
   }
+  const currentTerm = terms[0].termId;
+
+  macros.log('The current term is:', currentTerm);
+
+  // Add the classes
+  const courses = await Course.findAll({
+    attributes: ['subject', 'classId', 'name'],
+    where: { termId: currentTerm },
+    raw: true,
+  });
+  for (const course of courses) {
+    items.push(`${course.subject} ${course.classId}`);
+    items.push(course.name);
+  }
+
+  // Add the employees
+  const employees = await Professor.findAll({
+    attributes: ['name'],
+    benchmark: true,
+    raw: true,
+  });
+  for (const employee of employees) {
+    items.push(employee.name);
+  }
+
+  // Remove duplicates
+  items = Array.from(new Set(items));
 
   // Convert the items to urls and put them inside xml
   const xml = ['<?xml version="1.0" encoding="UTF-8"?>', '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'];
