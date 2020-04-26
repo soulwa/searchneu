29abe0fa36449e025093891552922a430cb7a06c
gamburgm <mitch.gamburg@gmail.com>
2020-04-26 17:43:51 -0400

    corrections

--- backend/dumpProcessor.js
+++ backend/dumpProcessor.js
@@ -38,9 +38,7 @@ class DumpProcessor {
       const processedChunk = classChunk.map((aClass) => { return this.processClass(aClass, coveredTerms); });
       return Course.bulkCreate(processedChunk, { updateOnDuplicate: courseAttributes });
     });
-    const stuff = await Promise.all(classPromises);
-    console.log(stuff);
-    const courseInstances = stuff.flat();
+    const courseInstances = (await Promise.all(classPromises)).flat();
 
     const secPromises = _.chunk(termDump.sections, this.CHUNK_SIZE).map(async (secChunk) => {
       const processedChunk = secChunk.map((section) => { return this.processSection(section); });
