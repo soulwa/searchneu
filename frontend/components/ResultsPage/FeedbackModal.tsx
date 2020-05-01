6d8684caef75f968922ba9188a7194709fd37e6e
Amiel Monasterial <amielmon@gmail.com>
2020-05-01 18:04:31 -0400

    start

--- frontend/components/ResultsPage/FeedbackModal.tsx
+++ frontend/components/ResultsPage/FeedbackModal.tsx
@@ -4,10 +4,75 @@ import CheckboxGroup from './CheckboxGroup';
 import macros from '../macros';
 import useFeedbackSchedule from './useFeedbackSchedule';
 
+interface InitialStepProps {
+  yes: boolean;
+  setYes: (b:boolean) => void;
+}
+
+
+function FeedbackModalInitial({ yes, setYes } : InitialStepProps) {
+  return (
+    <>
+      <div className='FeedbackModal__popoutSubHeader'>
+        <p>Did you find what you were looking for?</p>
+      </div>
+      <div className='FeedbackModal__initial'>
+          <label className='FeedbackModal_initialText' onClick={() => setYes(true)}>
+            <input type='radio' name='FeedbackRadio'/>
+            <span className='FeedbackModal__initialRadio' />
+            Yes
+          </label>
+          <label className='FeedbackModal__initialText' onClick={() => setYes(false)}>
+            <input type='radio' name='FeedbackRadio'/>
+            <span className='FeedbackModal__initialRadio' />
+            No
+          </label>
+      </div>
+    </>
+
+
+  );
+}
+
+interface CheckboxProps {
+  feedbackOptions: string[];
+  selectedFeedback: string[];
+  setSelectedFeedback: (a:string[]) => void;
+}
+
+function FeedbackModalCheckboxes({ feedbackOptions, selectedFeedback, setSelectedFeedback } : CheckboxProps) {
+  return (
+    <>
+      <div className='FeedbackModal__popoutSubHeader'>
+        <p>What info are you looking for?</p>
+      </div>
+      <div className='FeedbackModal__checkBoxes'>
+        <CheckboxGroup name='FeedbackModalCheckboxes' value={ selectedFeedback } onChange={ setSelectedFeedback }>
+          {(Checkbox) => (
+            <>
+              {feedbackOptions.map((feedbackOption) => (
+                <div key={ feedbackOption } className='FeedbackModal__checkboxElement'>
+                  <label className='FeedbackModal__checkboxText'>
+                    <Checkbox value={ feedbackOption } />
+                    <span className='FeedbackModal__checkboxBox' />
+                    {feedbackOption}
+                  </label>
+                </div>
+              ))}
+            </>
+          )}
+        </CheckboxGroup>
+      </div>
+    </>
+  );
+}
+
 export default function FeedbackModal() {
   const [open, setOpen] = useState(false);
+  const [yes, setYes] = useState(false);
   const [selectedFeedback, setSelectedFeedback] = useState([]);
   const [submitted, setSubmitted] = useState(false);
+  const [step, setStep] = useState('initial');
   const modalRef = useRef(null);
   const keyString = 'MODAL'
   const [show, setFinished] = useFeedbackSchedule(keyString, 86400000);
@@ -19,6 +84,15 @@ export default function FeedbackModal() {
       return;
     }
     setOpen(false);
+  }
+  console.log('yes', yes);
+  const renderFeedback = (step) => {
+    switch (step) {
+      case 'initial':
+        return <FeedbackModalInitial yes={ yes } setYes={ setYes } />;
+      case 'checkbox':
+        return <FeedbackModalCheckboxes feedbackOptions={ feedbackOptions } selectedFeedback={ feedbackOptions } setSelectedFeedback={ setSelectedFeedback } />;
+    }
   }
 
   useEffect(() => {
@@ -42,26 +116,7 @@ export default function FeedbackModal() {
         <div className='FeedbackModal__popoutHeader'>
           <p>SearchNEU Feedback</p>
         </div>
-        <div className='FeedbackModal__popoutSubHeader'>
-          <p>What info are you looking for?</p>
-        </div>
-        <div className='FeedbackModal__checkBoxes'>
-          <CheckboxGroup name='FeedbackModalCheckboxes' value={ selectedFeedback } onChange={ setSelectedFeedback }>
-            {(Checkbox) => (
-              <>
-                {feedbackOptions.map((feedbackOption) => (
-                  <div key={ feedbackOption } className='FeedbackModal__checkboxElement'>
-                    <label className='FeedbackModal__checkboxText'>
-                      <Checkbox value={ feedbackOption } />
-                      <span className='FeedbackModal__checkboxBox' />
-                      {feedbackOption}
-                    </label>
-                  </div>
-                ))}
-              </>
-            )}
-          </CheckboxGroup>
-        </div>
+        { renderFeedback(step)}
         <div className={ !submitted ? 'FeedbackModal__submit' : 'FeedbackModal__submit--submitted' } role='button' tabIndex={ 0 } onClick={ () => { setSubmitted(true); setFinished(); macros.logAmplitudeEvent('Feedback modal submit', { lookingFor: selectedFeedback }); } }>
           <p>{!submitted ? 'SEND FEEDBACK' : 'THANK YOU!' }</p>
         </div>
