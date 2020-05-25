7a82befced9619caac3b0b13057b8068bbd3f6d1
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-05-25 14:47:54 -0400

    Turn select/option into divs.

--- frontend/components/ResultsPage/DropdownFilter.tsx
+++ frontend/components/ResultsPage/DropdownFilter.tsx
@@ -1,37 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import { pull } from 'lodash';
 import { Option } from './filters';
 
 interface DropdownFilter {
   title: string,
   options: Option[],
   selected: string[],
-  setActive: (a:string)=>void
+  setActive: (a:string[])=>void
 }
 export default function DropdownFilter({
   title, options, selected, setActive,
 }: DropdownFilter) {
   const [areOptionsFresh, setAreOptionsFresh] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
   useEffect(() => setAreOptionsFresh(true), [options])
 
-  function handleClickOnTheDropdown(e: React.FormEvent<HTMLSelectElement>) {
+  function handleClickOnTheDropdown() {
     if (areOptionsFresh) {
       // calculate the new options
-      setActive(e.currentTarget.value);
+      // if any selected no longer exist, remove them (filter)
       setAreOptionsFresh(false)
     }
   }
 
   return (
     <div className='DropdownFilter'>
-      <span className='filter__title'>{title}</span>
+      <div className='filter__title'>{title}</div>
       <div className='DropdownFilter'>
-        <select value={ selected } onChange={ handleClickOnTheDropdown }>
+        <input className='DropdownFilter__input' tabIndex={ 0 } type='text' />
+        <div className='DropdownFilter__selectable'>
           {options.map(option => (
-            <option key={ option.value } value={ option.value }>
-              {option.value}
-            </option>
+            <div
+              role='option'
+              tabIndex={ -1 }
+              aria-selected='true'
+              aria-checked='false'
+              className='DropdownFilter__element'
+              key={ option.value }
+              onClick={ () => { setActive(selected.includes(option.value) ? pull(selected, option.value) : [...selected, option.value]) } }
+            >
+              <span className='DropdownFilter__elementText'>{option.value}</span>
+              <span className='DropdownFilter__elementCount'>{option.count}</span>
+            </div>
           ))}
-        </select>
+
+        </div>
       </div>
     </div>
   );
