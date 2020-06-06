050ceca5e872237e4f2ddad4a873c179cfd4f685
Amiel Monasterial <amielmon@gmail.com>
2020-06-06 18:02:32 -0400

    mobilio

--- frontend/components/ResultsPage/Results/MobileSearchResult.tsx
+++ frontend/components/ResultsPage/Results/MobileSearchResult.tsx
@@ -16,6 +16,11 @@ interface MobileSearchResultProps {
 function MobileSearchResult({aClass, history} : MobileSearchResultProps) {
 
   const [expanded, setExpanded] = useState(false)
+  const [showMore, setShowMore] = useState(false)
+  const [showNUPath, setShowNUPath] = useState(false)
+  const [showPrereq, setShowPrereq] = useState(false)
+  const [showCoreq, setShowCoreq] = useState(false)
+  const optionalDisplay = useResultRequisite(history);
   
   return (
     <div className='MobileSearchResult'>
@@ -26,6 +31,39 @@ function MobileSearchResult({aClass, history} : MobileSearchResultProps) {
       </span>
       </div>
       {expanded && <div className='MobileSearchResult__panel'>
+        <div className='MobileSearchResult__panel--container'>
+        <div className='MobileSearchResult__panel--infoStrings'>
+        <a href={ aClass.prettyUrl }>{`Updated ${(aClass.getLastUpdateString())}`}</a>
+        <span>
+          {aClass.maxCredits === aClass.minCredits ? `${aClass.maxCredits} Credits` : `${aClass.maxCredits}-${aClass.maxCredits} Credits`}
+        </span>
+        </div>
+        <div className={showMore ? 'MobileSearchResult__panel--description' : 'MobileSearchResult__panel--descriptionHidden'}>
+          {aClass.desc}
+        </div>
+        <div className='MobileSearchResult__panel--showMore' onClick={() => setShowMore(!showMore)}>{showMore ? 'Show less' : 'Show more'}</div>
+        <div className='MobileSearchResult__panel--collapsableContainer'>
+          <div className='MobileSearchResult__panel--collapsableTitle' onClick={() => setShowNUPath(!showNUPath)}>
+          <IconCollapseExpand width={'6'} height={'12'} fill={'#000000'} className={showNUPath && 'MobileSearchResult__panel--rotatedIcon'}/>
+          <span>NUPATHS</span>
+          </div>
+          {showNUPath && <div>{aClass.nupath.length > 0 ? <div> {aClass.nupath.join(', ')}</div> : <span className='empty'> None</span>}</div>}
+        </div>
+        <div className='MobileSearchResult__panel--collapsableContainer' onClick={() => setShowPrereq(!showPrereq)}>
+          <div className='MobileSearchResult__panel--collapsableTitle'>
+          <IconCollapseExpand width={'6'} height={'12'} fill={'#000000'} className={showPrereq && 'MobileSearchResult__panel--rotatedIcon'}/>
+          <span>PREREQUISITES</span>
+          </div>
+          {showPrereq && <div>{optionalDisplay(macros.prereqTypes.PREREQ, aClass)}</div>}
+        </div>
+        <div className='MobileSearchResult__panel--collapsableContainer' onClick={() => setShowCoreq(!showCoreq)}>
+          <div className='MobileSearchResult__panel--collapsableTitle'>
+          <IconCollapseExpand width={'6'} height={'12'} fill={'#000000'} className={showCoreq && 'MobileSearchResult__panel--rotatedIcon'}/>
+          <span>COREQUISITES</span>
+          </div>
+          {showCoreq && <div>{optionalDisplay(macros.prereqTypes.COREQ, aClass)}</div>}
+        </div>
+        </div>
       </div>}
     </div>
 
