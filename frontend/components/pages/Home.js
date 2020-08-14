df6d2c2872e0fc2899e8a82729b19f85cada82d2
Rebekah Johnson <johnson.re@husky.neu.edu>
2020-08-14 17:16:26 -0400

    removes ya

--- frontend/components/pages/Home.js
+++ frontend/components/pages/Home.js
@@ -114,7 +114,7 @@ export default function Home() {
             </div>
             {attentionSection}
           </div>
-        </div>ya
+        </div>
         <SplashPage />
         <Footer />
       </div>
