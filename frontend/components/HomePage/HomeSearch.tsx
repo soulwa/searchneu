8e7f66e66b2bb9e707b2631d4cc6a32a44ee5749
Amiel Monasterial <amielmon@gmail.com>
2020-08-16 17:42:28 -0400

    icon

--- frontend/components/HomePage/HomeSearch.tsx
+++ frontend/components/HomePage/HomeSearch.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import SearchBar from '../ResultsPage/SearchBar'
-import TermDropdown from '../ResultsPage/TermDropdown';
+import TermDropdown from '../ResultsPage/TermDropdown'
+import IconGradcap from '../images/IconGradcap'
+import IconScale from '../images/IconScale'
+import IconTie from '../images/IconTie'
 
 
 interface HomeSearchProps {
@@ -10,7 +13,7 @@ interface HomeSearchProps {
   termId: string
 }
 
-const HomeSearch = ({setSearchFocused, setTermId, termId}) => {
+const HomeSearch = ({ setTermId, termId}) => {
   const history = useHistory();
   const [selectedCampus, setSelectedCampus] = useState('neu');
   console.log('selectedCampus', selectedCampus)
@@ -20,15 +23,18 @@ const HomeSearch = ({setSearchFocused, setTermId, termId}) => {
       <div className='HomeSearch__campusSelector'>
           <input type='radio' id='campusSelectorNeu' name='CampusSelector' defaultChecked/>
           <label className='HomeSearch__campusSelector--neu' htmlFor='campusSelectorNeu' onClick={ () => setSelectedCampus('neu') }>
+          <IconGradcap/>
           <span>NEU</span>
           </label>
           <input type='radio' id='campusSelectorCps' name='CampusSelector' />
           <label className='HomeSearch__campusSelector--cps' htmlFor='campusSelectorCps' onClick={ () => setSelectedCampus('cps') }>
+            <IconTie/>
             <span>CPS</span>
           </label>
           <input type='radio' id='campusSelectorLaw' name='CampusSelector' />
           <label className='HomeSearch__campusSelector--law' htmlFor='campusSelectorLaw' onClick={ () => setSelectedCampus('law') }>
-            <span>Law</span>
+            <IconScale/>
+            <span>Pee</span>
           </label>
       </div>
       <div className='HomeSearch__searchBar'>
