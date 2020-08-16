c9a8aaebcd01d7083daeb3e2268bff129c7597c9
Amiel Monasterial <amielmon@gmail.com>
2020-08-16 17:45:39 -0400

    icon

--- frontend/components/HomePage/HomeSearch.tsx
+++ frontend/components/HomePage/HomeSearch.tsx
@@ -8,53 +8,51 @@ import IconTie from '../images/IconTie'
 
 
 interface HomeSearchProps {
-  setSearchFocused: (b: boolean) => void
   setTermId: (s: string) => void
   termId: string
 }
 
-const HomeSearch = ({ setTermId, termId}) => {
+const HomeSearch = ({ setTermId, termId }: HomeSearchProps) => {
   const history = useHistory();
   const [selectedCampus, setSelectedCampus] = useState('neu');
-  console.log('selectedCampus', selectedCampus)
 
   return (
     <div className='HomeSearch'>
       <div className='HomeSearch__campusSelector'>
-          <input type='radio' id='campusSelectorNeu' name='CampusSelector' defaultChecked/>
-          <label className='HomeSearch__campusSelector--neu' htmlFor='campusSelectorNeu' onClick={ () => setSelectedCampus('neu') }>
-          <IconGradcap/>
+        <input type='radio' id='campusSelectorNeu' name='CampusSelector' defaultChecked />
+        <label className='HomeSearch__campusSelector--neu' htmlFor='campusSelectorNeu' onClick={ () => setSelectedCampus('neu') }>
+          <IconGradcap />
           <span>NEU</span>
-          </label>
-          <input type='radio' id='campusSelectorCps' name='CampusSelector' />
-          <label className='HomeSearch__campusSelector--cps' htmlFor='campusSelectorCps' onClick={ () => setSelectedCampus('cps') }>
-            <IconTie/>
-            <span>CPS</span>
-          </label>
-          <input type='radio' id='campusSelectorLaw' name='CampusSelector' />
-          <label className='HomeSearch__campusSelector--law' htmlFor='campusSelectorLaw' onClick={ () => setSelectedCampus('law') }>
-            <IconScale/>
-            <span>Pee</span>
-          </label>
+        </label>
+        <input type='radio' id='campusSelectorCps' name='CampusSelector' />
+        <label className='HomeSearch__campusSelector--cps' htmlFor='campusSelectorCps' onClick={ () => setSelectedCampus('cps') }>
+          <IconTie />
+          <span>CPS</span>
+        </label>
+        <input type='radio' id='campusSelectorLaw' name='CampusSelector' />
+        <label className='HomeSearch__campusSelector--law' htmlFor='campusSelectorLaw' onClick={ () => setSelectedCampus('law') }>
+          <IconScale />
+          <span>Pee</span>
+        </label>
       </div>
       <div className='HomeSearch__searchBar'>
         <div className='HomeSearch__searchBar--dropdown'>
           <TermDropdown
             termId={ termId }
             onChange={ setTermId }
-            compact={false}
+            compact={ false }
           />
         </div>
-        <div 
+        <div
           className='HomeSearch__searchBar--input'
         >
           <SearchBar
-            onSearch={ (q) => { history.push(`/${termId}/${q}`); } }
+            onSearch={ (q) => { history.push(`${selectedCampus}/${termId}/${q}/`); } }
             query=''
           />
         </div>
       </div>
-  </div>
+    </div>
 
 
   )
--- frontend/components/images/IconTie.tsx
+++ frontend/components/images/IconTie.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 export default ({
-  width = "10", height = "21", className
+  width = '10', height = '21', className,
 } : {
   width?: string, height?: string, className?: string}) => (
-  <svg width={width} height={height} className={className} viewBox="0 0 10 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1.08219 16.9401L4.94521 19.9288C5 19.9763 5.05479 20 5.13699 20C5.19178 20 5.27397 19.9763 5.32877 19.9288L8.91781 16.9875C8.9726 16.9401 9 16.8689 9 16.774L7.10959 5.41199C7.54794 
-    5.36454 7.93151 5.05618 7.9863 4.65293L8.36986 1.90137C8.39726 1.66417 8.31507 1.45069 8.15068 1.26092C7.9863 1.09488 7.71233 1 7.43836 1H2.58904C2.31507 1 2.06849 1.09488 1.87671 1.26092C1.71233 
-    1.42697 1.60274 1.66417 1.65753 1.90137L2.0411 4.65293C2.09589 5.05618 2.45205 5.36454 2.91781 5.41199L1 16.7266C1 16.8215 1.0274 16.8926 1.08219 16.9401Z" stroke="#DA6D30" stroke-miterlimit="10"/>
-    <path d="M1.5 14L9 17" stroke="#DA6D30" stroke-width="0.5"/>
-    <path d="M3 6.5L7.5 8" stroke="#DA6D30" stroke-width="0.5"/>
-    <path d="M2.5 9L8 11" stroke="#DA6D30" stroke-width="0.5"/>
-    <path d="M2 11.5L8.5 14" stroke="#DA6D30" stroke-width="0.5"/>
-  </svg>
+    <svg width={ width } height={ height } className={ className } viewBox='0 0 10 21' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M1.08219 16.9401L4.94521 19.9288C5 19.9763 5.05479 20 5.13699 20C5.19178 20 5.27397 19.9763 5.32877 19.9288L8.91781 16.9875C8.9726 16.9401 9 16.8689 9 16.774L7.10959 5.41199C7.54794
+    5.36454 7.93151 5.05618 7.9863 4.65293L8.36986 1.90137C8.39726 1.66417 8.31507 1.45069 8.15068 1.26092C7.9863 1.09488 7.71233 1 7.43836 1H2.58904C2.31507 1 2.06849 1.09488 1.87671 1.26092C1.71233
+    1.42697 1.60274 1.66417 1.65753 1.90137L2.0411 4.65293C2.09589 5.05618 2.45205 5.36454 2.91781 5.41199L1 16.7266C1 16.8215 1.0274 16.8926 1.08219 16.9401Z'
+        stroke='#DA6D30'
+        strokeMiterlimit='10'
+      />
+      <path d='M1.5 14L9 17' stroke='#DA6D30' strokeWidth='0.5' />
+      <path d='M3 6.5L7.5 8' stroke='#DA6D30' strokeWidth='0.5' />
+      <path d='M2.5 9L8 11' stroke='#DA6D30' strokeWidth='0.5' />
+      <path d='M2 11.5L8.5 14' stroke='#DA6D30' strokeWidth='0.5' />
+    </svg>
 )
